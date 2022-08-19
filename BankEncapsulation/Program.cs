49e4d5ae18400b1a7dccf4fd47011dcc9467c98b
Derek Griffin <griffinderek6@gmail.com>
2022-08-19 10:28:10 -0600

    finished

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+
+            Console.WriteLine("How much would you like to depoist");
+
+            var amountToDepoist = double.Parse(Console.ReadLine());
+
+            account.Depoist(amountToDepoist);
+
+            Console.WriteLine($"Your balance is now {account.GetBalance()}");
+        }
+    }
+}
+
